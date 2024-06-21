b8e08de08727a074339efa6c08c5f458ba089440
juhaaan <iwantbelieve_kk@yahoo.com>
2024-06-21 19:24:33 +0300

    create new cpp file called just nothing cpp

---
+++ just_nothing.cpp
@@ -0,0 +1,7 @@
+//dont forget to clone existing github repo to your machine, dude
+#include <iostream>
+
+int main() {
+    std::cout<< "hi there, dude, check that reminder above";
+    return 0;
+}
