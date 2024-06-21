76898d39423f55f53be08441cf4f1ba293561126
juhaaan <iwantbelieve_kk@yahoo.com>
2024-06-21 21:54:41 +0300

    delete cpp file

--- just_nothing.cpp
+++
@@ -1,8 +0,0 @@
-//dont forget to clone existing github repo to your machine, dude
-#include <iostream>
-
-int main() {
-    std::cout<< "hi there, dude, check that reminder above";
-    std::cout<< "ok, i will clone remote repo to my machine, thanks for reminder, me in the past";
-    return 0;
-}
